44f26e67ae4ddbbdf0269a4215b0dd0e42d0dee9
Ana <anajuliasn.2007@gmail.com>
2024-08-28 14:32:55 -0300

    continuação menu

--- manu.js
+++ manu.js
@@ -1,5 +1,7 @@
 
 window.addEventListener("scroll", function(){
     let header = document.querySelector('#header')
-    header.classList.toggle('rolagem', window.scrollY > 0)
+    header.classList.toggle('rolagem', window.scrollY > 500)
+    let hr = document.querySelector('#hr')
+    hr.classList.toggle('rolagem', window.scrollY > 500)
 })
