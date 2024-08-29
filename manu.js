2b74528741de4f98c78e9fd80de4ec3de7f40b7e
Ana <anajuliasn.2007@gmail.com>
2024-08-28 22:50:27 -0300

    "Finalização" menu

--- manu.js
+++ manu.js
@@ -1,7 +1,15 @@
+window.addEventListener("scroll", function() {
+    let header = document.querySelector('#header');
+    let logoBranca = document.getElementById('logo-branca');
+    let logoAzul = document.getElementById('logo-azul');
 
-window.addEventListener("scroll", function(){
-    let header = document.querySelector('#header')
-    header.classList.toggle('rolagem', window.scrollY > 500)
-    let hr = document.querySelector('#hr')
-    hr.classList.toggle('rolagem', window.scrollY > 500)
-})
+    if (window.scrollY > 500) {
+        header.classList.add('rolagem');
+        logoBranca.style.display = 'none'; // Oculta a logo branca
+        logoAzul.style.display = 'block'; // Mostra a logo azul
+    } else {
+        header.classList.remove('rolagem');
+        logoBranca.style.display = 'block'; // Mostra a logo branca
+        logoAzul.style.display = 'none'; // Oculta a logo azul
+    }
+});
