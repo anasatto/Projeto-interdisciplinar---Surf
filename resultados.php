979e8d648add092ee948ee8daf3d4d7d5fc019cc
Ana <anajuliasn.2007@gmail.com>
2024-09-08 22:41:34 -0300

    formulário e php

--- resultados.php
+++ resultados.php
@@ -1,3 +1,19 @@
 <?php
     $nome = addcslashes($_POST['nome']);
+    $email = addcslashes($_POST['email']);
+    $mensagem = addcslashes($_POST['mensagem']);
+
+
+    $para = "anajuliasn.2007@gmail.com";
+    $assunto = "Coleta de dados - Crista da Onda";
+
+    $corpo = "Nome: ".$nome."\n"."E-mail: ".$email."\n"."Mensagem: ".$mansagem;
+
+    $cabeca = "From: anajuliasn.2007@gmail.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
+
+    if(mail($para,$assunto,$corpo,$cabeca)){
+        echo(E-mail enviado com sucesso!);
+    }else{
+        echo(Houve um erro ao enviar o e-mail!);
+    }
 ?>
