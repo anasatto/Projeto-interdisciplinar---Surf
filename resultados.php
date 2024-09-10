50aff291fea3d20722fc0e8207796264f10e5cf2
Ana <anajuliasn.2007@gmail.com>
2024-09-10 18:22:43 -0300

    BD

--- resultados.php
+++ resultados.php
@@ -1,5 +1,5 @@
 <?php
-    // Sanitização dos dados
+    //Sanitização dos dados
     $nome = addslashes(trim($_POST['nome']));
     $email = filter_var(trim($_POST['email']), FILTER_SANITIZE_EMAIL);
     $mensagem = addslashes(trim($_POST['mensagem']));
@@ -13,16 +13,29 @@
     $para = "anajuliasn.2007@gmail.com";
     $assunto = "Coleta de dados - Crista da Onda";
 
-    // Corpo do e-mail
+    //Corpo do e-mail
     $corpo = "Nome: ".$nome."\nE-mail: ".$email."\nMensagem: ".$mensagem;
 
-    // Cabeçalho do e-mail
+    //Cabeçalho do e-mail
     $cabeca = "From: anajuliasn.2007@gmail.com"."\nReply-to: ".$email."\nX-Mailer: PHP/".phpversion();
 
-    // Envio do e-mail
-    if(mail($para, $assunto, $corpo, $cabeca)){
+    //Envio do e-mail
+    if(email($para, $assunto, $corpo, $cabeca)){
         echo "E-mail enviado com sucesso!";
     } else {
         echo "Houve um erro ao enviar o e-mail!";
     }
 ?>
+
+
+// if(isset($_POST['nome']) or isset($_POST['email']) or isset($_POST['mensagem'])){
+//     $nome = $_POST['nome'];
+//     $email = $_POST['email'];
+//     $mensagem = $_POST['mensagem'];
+
+//     echo $nome;
+//     echo $email;
+//     echo $mansagem;
+// }
+
+// ?>
