9196cf1c3ba927a23e9b520640d8739e61880080
Ana <anajuliasn.2007@gmail.com>
2024-09-15 00:05:45 -0300

    atualização php

--- resultados.php
+++ resultados.php
@@ -1,28 +1,12 @@
 <?php
-    //Sanitização dos dados
-    $nome = addslashes(trim($_POST['nome']));
-    $email = filter_var(trim($_POST['email']), FILTER_SANITIZE_EMAIL);
-    $mensagem = addslashes(trim($_POST['mensagem']));
+if(isset($_POST["nome"]) or isset($_POST["email"])){
+        $nome = $_POST["nome"];
+        $email = $_POST["email"];
+        $mensagem = $_POST["mensagem"];
+echo "<h2>Mensagem recebida com sucesso!</h2>";
+echo "Usuário: $nome <br>";
+echo "E-Mail: $email <br>";
+echo "Mensagem: $mensagem";
 
-    // Validação do e-mail
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        echo "E-mail inválido!";
-        exit;
-    }
-
-    $para = "anajuliasn.2007@gmail.com";
-    $assunto = "Coleta de dados - Crista da Onda";
-
-    //Corpo do e-mail
-    $corpo = "Nome: ".$nome."\nE-mail: ".$email."\nMensagem: ".$mensagem;
-
-    //Cabeçalho do e-mail
-    $cabeca = "From: anajuliasn.2007@gmail.com"."\nReply-to: ".$email."\nX-Mailer: PHP/".phpversion();
-
-    //Envio do e-mail
-    if(email($para, $assunto, $corpo, $cabeca)){
-        echo "E-mail enviado com sucesso!";
-    } else {
-        echo "Houve um erro ao enviar o e-mail!";
-    }
-?> 
+}
+?>
